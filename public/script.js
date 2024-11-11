71452e09d7ab29a60c3768b3722bcf9955aa3054
RizikH <rihaddad2@uncg.edu>
2024-11-10 23:59:35 -0500

    e

--- public/script.js
+++ public/script.js
@@ -2,9 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   search()
-//   const form = document.getElementById('jokeForm')
-//   form.addEventListener('submit', handleFormSubmission)
-})
+});
 
 function search () {
   const searchBar = document.querySelector('.search-bar')
@@ -14,37 +12,3 @@ function search () {
     }
   })
 }
-
-// function handleFormSubmission (event) {
-//   event.preventDefault()
-
-//   const category = document.getElementById('category').value
-//   const setup = document.getElementById('setup').value
-//   const delivery = document.getElementById('delivery').value
-
-//   const data = {
-//     category: category,
-//     setup: setup,
-//     delivery: delivery
-//   }
-
-//   fetch('http://localhost:3000/jokebook/new', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(data)
-//   })
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`)
-//       }
-//       return response.json()
-//     })
-//     .then(data => {
-//       console.log('Success:', data)
-//     })
-//     .catch(error => {
-//       console.error('Error:', error)
-//     })
-// }
